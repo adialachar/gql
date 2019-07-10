02b4de6c5bbc1e055d7650cf6d8529b47370ae2d
Aditya Acharya <aacha002@ucr.edu>
2019-07-09 23:38:22 -0700

    relay

--- cookbook/ingredients/schema.py
+++ cookbook/ingredients/schema.py
@@ -1,25 +1,40 @@
-import graphene
+from graphene import relay, ObjectType
 
 from graphene_django.types import DjangoObjectType
-
+from graphene_django.filter import DjangoFilterConnectionField
 from cookbook.ingredients.models import Category, Ingredient
 
-class CategoryType(DjangoObjectType):
+class CategoryNode(DjangoObjectType):
     class Meta:
         model = Category
+        filter_fields = ['name', 'ingredients']
+        interfaces = (relay.Node, )
 
-class IngredientType(DjangoObjectType):
+class IngredientNode(DjangoObjectType):
     class Meta:
         model = Ingredient
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'notes': ['exact', 'icontains'],
+            'category': ['exact'],
+            'category__name': ['exact'],
+        }
+        interfaces = (relay.Node, )
 
-class Query(object):
+class Query(ObjectType):
 
-    all_categories = graphene.List(CategoryType)
-    all_ingredients = graphene.List(IngredientType)
+    category = relay.Node.Field(CategoryNode)
+    all_categories = DjangoFilterConnectionField(CategoryNode)
+    #all_ingredients = graphene.List(IngredientType)
 
-    def resolve_all_categories(self, info, **kwargs):
-        return Category.objects.all()
+    #def resolve_all_categories(self, info, **kwargs):
+    #    return Category.objects.all()
 
-    def resolve_all_ingredients(self, info, **kwargs):
+    #def resolve_all_ingredients(self, info, **kwargs):
 
-        return Ingredient.objects.select_related('category').all()
+     #   return Ingredient.objects.select_related('category').all()
+
+
+    ingredient = relay.Node.Field(IngredientNode)
+    all_ingredients = DjangoFilterConnectionField(IngredientNode)
+
