b4b2cc742f9746df59abc581125cfb1656dcf5c3
Aditya Acharya <aacha002@ucr.edu>
2019-07-15 22:05:54 -0700

    JsonResponse Sucessful

--- ucr/views.py
+++ ucr/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
 
 
 def index(request):
 
-    return render(request, 'index.html', context={})
+    #return render(request, 'index.html', context={})
+    return JsonResponse({"Success":True})
