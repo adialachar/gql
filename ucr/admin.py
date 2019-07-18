44f5ad7dc2e0bc23182f54dd41d79c7aa4ffb4f0
Aditya Acharya <aacha002@ucr.edu>
2019-07-17 22:52:45 -0700

    gave admin models

--- ucr/admin.py
+++ ucr/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 #from .models import MyUser, Profile
-from .models import UserModel
+#from .models import UserModel
 # Register your models here.
-#admin.site.register(MyUser)
-#admin.site.register(Profile)
-admin.site.register(UserModel)
+admin.site.register(MyUser)
+admin.site.register(Profile)
+#admin.site.register(UserModel)
